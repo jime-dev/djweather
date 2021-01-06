efcb7fad5305403cf74ddb91184fd3d83f2a796a
coder-dajin <1748723423@qq.com>
2021-01-06 09:27:57 +0800

    加入创建数据库的表和各项配置

---
+++ app/src/main/java/com/djweather/android/db/County.java
@@ -0,0 +1,43 @@
+package com.djweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+
+    private int id;
+    private String countyName; // 记录县城名称
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
